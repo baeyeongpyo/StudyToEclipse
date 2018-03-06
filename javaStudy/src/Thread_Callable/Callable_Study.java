f727d89d33ecbf7d307d45e057270d9f942b57f4
yeongpyo <byp93@naver.com>
2018-03-06 23:36:43 +0900

    2018-03-06-23:36

---
+++ javaStudy/src/Thread_Callable/Callable_Study.java
@@ -0,0 +1,16 @@
+package Thread_Callable;
+
+import java.util.ArrayList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class Callable_Study {
+	public void Study_class() {
+		ExecutorService exector = Executors.newFixedThreadPool(2);
+		ArrayList<Future<String>> arraylist = new ArrayList<>();
+		
+		
+		
+	}
+}
---
+++ javaStudy/src/Thread_Callable/Callable_one.java
@@ -0,0 +1,13 @@
+package Thread_Callable;
+
+import java.util.concurrent.Callable;
+
+public class Callable_one implements Callable<String>{
+
+	@Override
+	public String call() throws Exception {
+		
+		return null;
+	}
+
+}
